0736dbb760d7055096db99367a34aeb645888120
Ayman Abu Awad <58958197+ayman-bit@users.noreply.github.com>
2022-06-16 18:06:30 -0230

    adding:  - Create Elevator and Rider classes  - Implemented randomNum function on ElevatorRiderFactory

---
+++ src/mun/concurrent/assignment/two/Elevator.java
@@ -0,0 +1,17 @@
+package mun.concurrent.assignment.two;
+
+public class Elevator {
+    private final int capacity;
+    private final int currentCount;
+    private final int currentFloor;
+
+    public Elevator(int capacity, int currentCount, int currentFloor){
+        // TODO: add emun status
+
+        this.capacity = capacity;
+        this.currentCount = currentCount;
+        this.currentFloor = currentFloor;
+    }
+
+
+}
--- src/mun/concurrent/assignment/two/ElevatorRiderFactory.java
+++ src/mun/concurrent/assignment/two/ElevatorRiderFactory.java
@@ -1,4 +1,13 @@
 package mun.concurrent.assignment.two;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ElevatorRiderFactory {
+
+    public ElevatorRiderFactory() {}
+
+    public void nextRider(){
+        //TODO: create a Rider object
+        int randomNum = ThreadLocalRandom.current().nextInt(20, 120 + 1);
+        System.out.println(randomNum);
+    }
 }
